31716174a935e58e1b84159bbb51f1a35bfdd0e6
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-05-08 16:39:24 -0700

    added a space in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
+
 about.addEventListener("click", function (e) {
     const id = e.target.dataset.id;
     if (id) {
